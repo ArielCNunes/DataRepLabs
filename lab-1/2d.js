30db842d0d868d2e291893f90104b48d2f1d8006
Ariel <arielc.nunes@outlook.com>
2024-09-27 13:32:48 +0000

    Created new files for each exercise. Started 2d

---
+++ lab-1/2d.js
@@ -0,0 +1,9 @@
+// Numbers array
+let numbers = [25, 31, 42, 77];
+
+let multiply = () => {
+    return num1 + num2;
+}
+
+// Call sum and give it two numbers so that they can be summed
+console.log(sum(5, 5));
