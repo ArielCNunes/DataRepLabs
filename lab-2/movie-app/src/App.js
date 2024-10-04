cebe28e8368dee5370aa88874eb630af6007a725
Ariel <arielc.nunes@outlook.com>
2024-10-04 13:34:15 +0000

    Created new folder "components" and moved code from App.js to Content.js

--- lab-2/movie-app/src/App.js
+++ lab-2/movie-app/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <h2>It is {new Date().toLocaleDateString()}</h2>
+      
     </div>
   );
 }
