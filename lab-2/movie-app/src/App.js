63c6a8b9f83062827f85a9590c0152908345c73b
Ariel <arielc.nunes@outlook.com>
2024-10-04 14:28:39 +0000

    Q6 done - changed element in Route path

--- lab-2/movie-app/src/App.js
+++ lab-2/movie-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -14,8 +13,8 @@ function App() {
         <NavigationBar />
         <Routes>
           <Route path="/home" element={<Content />} />
-          <Route path="/read" element={<h1>Read Component</h1>} />
-          <Route path="/create" element={<h1>Create Component</h1>} />
+          <Route path="/read" element={<Footer />} />
+          <Route path="/create" element={<Header />} />
         </Routes>
         <Header />
         <Content />
